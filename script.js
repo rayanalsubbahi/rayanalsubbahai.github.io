30c34b3549b4fcf758148feef8ee71ce1201b4d4
rayan subbahi <rayan subbahi>
2015-11-29 12:35:09 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear(2016) + " rayans corporation. All rights reserved.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Costumers</a></li>" +
+"<li><a href='gallery.html'>Gallery</a></li>" +
+"<li><a href='future vision.html'>Future Vision</a></li>" +
+"<li><a href='careers.html'>Careers</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+
+"</ul>";
+document.getElementById("foot02").innerHTML =
+"<p>&copy;  " + new Date().getFullYear(2016) + " rayans corporation. All rights reserved.</p>";
